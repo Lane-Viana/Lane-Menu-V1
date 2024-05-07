72a4b768f2ed0d9ad06c4d9feb0ce0284b38a6cd
Lane-Viana <fenixnegra20022+1@gmail.com>
2024-05-07 18:09:11 -0300

    Update Lane Menu.lua

--- Lane Menu.lua
+++ Lane Menu.lua
@@ -1,21 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Lane Menu V1", "DarkTheme")
 
--- Loops
-local function LoopPlayerSpeed(s)
-    while true do
-	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-        wait(0.0001)
-    end
-end
-
-local function LoopPlayerJump(s)
-    while true do
-	game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
-        wait(0.0001)
-    end
-end
-
 -- Main
 local Main = Window:NewTab("Local")
 local MainSection = Main:NewSection("Local")
@@ -31,6 +16,21 @@ MainSection:NewSlider("JumpPower", "Jump Highest", 500, 50, function(s) -- 500 M
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
     LoopPlayerJump(s)
 end)
+
+-- Loops
+local function LoopPlayerSpeed(s)
+    while true do
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+        wait(0.0001)
+    end
+end
+
+local function LoopPlayerJump(s)
+    while true do
+	game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+        wait(0.0001)
+    end
+end
 
 -- Settings
 local Settings = Window:NewTab("Settings")
