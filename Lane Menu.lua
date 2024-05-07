a2828f7f13ce44a18b97020cda43c5cad57a22b8
Lane-Viana <fenixnegra20022+1@gmail.com>
2024-05-07 16:20:33 -0300

    Atualizado

--- Lane Menu.lua
+++ Lane Menu.lua
@@ -1,17 +1,11 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Lane Menu V1", "DarkTheme")
 
--- Tab
+-- Main
 local Main = Window:NewTab("Local")
-local Settings = Window:NewTab("Settings")
 
--- Label
 MainSection:NewLabel("Player")
 
--- Section
-local SettingsSection = Settings:NewSection("Menu Settings")
-
--- Functions & Keybinds
 MainSection:NewSlider("WalkSpeed", "Run fast", 350, 16, function(s) -- 350 Máximo | 16 Mínimo 
 game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
@@ -20,12 +14,14 @@ MainSection:NewSlider("JumpPower", "Jump Highest", 500, 50, function(s) -- 500 M
 game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
 end)
 
+-- Settings
+local Settings = Window:NewTab("Settings")
+local SettingsSection = Settings:NewSection("Menu Settings")
 
 SettingsSection:NewKeybind("Menu Open", "KeybindInfo", Enum.KeyCode.X, function()
 	Library:ToggleUI()
 end)
 
--- Button
 SettingsSection:NewButton("Boost FPS", "Boost your fps using anti lag script", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LegiteriumZ/RobloxScript/main/binh%20hub%20fps%20booster%20v1.lua"))()
 end)
